b4cfbb51c652fce517178585dc1aebe2d7a8e835
GuyGuy5000 <nadavhilu12@gmail.com>
2024-01-01 09:18:16 -0500

    first commit

---
+++ NadavHiluProjectPart4/Catering Management Solution/CateringManagement/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,178 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace CateringManagement.Data
+{
+    public static class ApplicationDbInitializer
+    {
+        public static async void Seed(IApplicationBuilder applicationBuilder)
+        {
+            ApplicationDbContext context = applicationBuilder.ApplicationServices.CreateScope()
+                .ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            try
+            {
+                //Create the database if it does not exist and apply the Migration
+                context.Database.Migrate();
+
+                //Create Roles
+                var RoleManager = applicationBuilder.ApplicationServices.CreateScope()
+                    .ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                string[] roleNames = { "Admin", "Security", "Supervisor", "Staff" };
+                IdentityResult roleResult;
+                foreach (var roleName in roleNames)
+                {
+                    var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                    {
+                        roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+                }
+                //Create Users
+                var userManager = applicationBuilder.ApplicationServices.CreateScope()
+                    .ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                if (userManager.FindByEmailAsync("admin@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "admin@outlook.com",
+                        Email = "admin@outlook.com",
+                        EmailConfirmed = true
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "Pa55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Admin").Wait();
+                        userManager.AddToRoleAsync(user, "Security").Wait();
+                    }
+                }
+
+                if (userManager.FindByEmailAsync("security@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "security@outlook.com",
+                        Email = "security@outlook.com",
+                        EmailConfirmed = true
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "Pa55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Security").Wait();
+                    }
+                }
+
+                if (userManager.FindByEmailAsync("supervisor@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "supervisor@outlook.com",
+                        Email = "supervisor@outlook.com",
+                        EmailConfirmed = true
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "Pa55w@rd").Result;
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Supervisor").Wait();
+                    }
+                }
+
+                if (userManager.FindByEmailAsync("staff@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "staff@outlook.com",
+                        Email = "staff@outlook.com",
+                        EmailConfirmed = true
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "Pa55w@rd").Result;
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Staff").Wait();
+                    }
+                }
+
+
+                if (userManager.FindByEmailAsync("user@outlook.com").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "user@outlook.com",
+                        Email = "user@outlook.com",
+                        EmailConfirmed = true
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "Pa55w@rd").Result;
+                    //No role added
+                }
+
+
+                //additional users
+                if (userManager.FindByEmailAsync("jkaluba@niagaracollege.ca").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "jkaluba@niagaracollege.ca",
+                        Email = "jkaluba@niagaracollege.ca",
+                        EmailConfirmed = true
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "Pa55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Admin").Wait();
+                        userManager.AddToRoleAsync(user, "Security").Wait();
+                    }
+                }
+
+                if (userManager.FindByEmailAsync("dstovell@niagaracollege.ca").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "dstovell@niagaracollege.ca",
+                        Email = "dstovell@niagaracollege.ca",
+                        EmailConfirmed = true
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "Pa55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Admin").Wait();
+                        userManager.AddToRoleAsync(user, "Security").Wait();
+                    }
+                }
+
+                if (userManager.FindByEmailAsync("nhilu1@ncstudents.niagaracollege.ca").Result == null)
+                {
+                    IdentityUser user = new IdentityUser
+                    {
+                        UserName = "nhilu1@ncstudents.niagaracollege.ca",
+                        Email = "nhilu1@ncstudents.niagaracollege.ca",
+                        EmailConfirmed = true
+                    };
+
+                    IdentityResult result = userManager.CreateAsync(user, "Pa55w@rd").Result;
+
+                    if (result.Succeeded)
+                    {
+                        userManager.AddToRoleAsync(user, "Admin").Wait();
+                        userManager.AddToRoleAsync(user, "Security").Wait();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.GetBaseException().Message);
+            }
+        }
+    }
+}
---
+++ NadavHiluProjectPart4/Catering Management Solution/CateringManagement/Utilities/MyEmailSender.cs
@@ -0,0 +1,102 @@
+﻿using CateringManagement.ViewModels;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace CateringManagement.Utilities
+{
+    public class MyEmailSender : IMyEmailSender
+    {
+        private readonly IEmailConfiguration _emailConfiguration;
+
+        public MyEmailSender(IEmailConfiguration emailConfiguration)
+        {
+            _emailConfiguration = emailConfiguration;
+        }
+
+        /// <summary>
+        /// Asynchronously builds and sends a message to a single recipient
+        /// </summary>        
+        /// <param name="name">Optional - Uses the email if not supplied</param>
+        /// <param name="email"></param>
+        /// <param name="subject"></param>
+        /// <param name="htmlMessage"></param>
+        /// <returns></returns>
+        public async Task SendOneAsync(string name, string email, string subject, string htmlMessage)
+        {
+            //only sends emails to this email for testing purposes
+            if (email != "nhilu1@ncstudents.niagaracollege.ca" || email != "jkaluba@niagaracollege.ca" || email != "dstovell@niagaracollege.ca")
+                return;
+
+            if (String.IsNullOrEmpty(name))
+            {
+                name = email;
+            }
+
+            var message = new MimeMessage();
+            message.To.Add(new MailboxAddress(name, email));
+            message.From.Add(new MailboxAddress(_emailConfiguration.SmtpFromName, _emailConfiguration.SmtpUsername));
+
+            message.Subject = subject;
+            //We will say we are sending HTML. But there are options for plaintext etc. 
+            message.Body = new TextPart(TextFormat.Html)
+            {
+                Text = htmlMessage
+            };
+
+            //Be careful that the SmtpClient class is the one from Mailkit not the framework!
+            using var emailClient = new SmtpClient();
+            //The last parameter here is to use SSL (Which you should!)
+            emailClient.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, false);
+
+            //Remove any OAuth functionality as we won't be using it. 
+            emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+
+            emailClient.Authenticate(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
+
+            await emailClient.SendAsync(message);
+
+            emailClient.Disconnect(true);
+        }
+
+        /// <summary>
+        /// Asynchronously sends a message to a List of email addresses
+        /// </summary>
+        /// <param name="emailMessage"></param>
+        /// <returns></returns>
+        public async Task SendToManyAsync(EmailMessage emailMessage)
+        {
+            //only sends emails to this email for testing purposes
+            emailMessage.ToAddresses = emailMessage.ToAddresses.Where(a => a.Address == "nhilu1@ncstudents.niagaracollege.ca").Select(a => a).ToList();
+            if (emailMessage.ToAddresses.Count() == 0)
+                return;
+
+
+            var message = new MimeMessage();
+            message.To.AddRange(emailMessage.ToAddresses.Select(x => new MailboxAddress(x.Name, x.Address)));
+
+            message.From.Add(new MailboxAddress(_emailConfiguration.SmtpFromName, _emailConfiguration.SmtpUsername));
+
+            message.Subject = emailMessage.Subject;
+            //We will say we are sending HTML. But there are options for plaintext etc. 
+            message.Body = new TextPart(TextFormat.Html)
+            {
+                Text = emailMessage.Content
+            };
+
+            //Be careful that the SmtpClient class is the one from Mailkit not the framework!
+            using var emailClient = new SmtpClient();
+            //The last parameter here is to use SSL (Which you should!)
+            emailClient.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.SmtpPort, false);
+
+            //Remove any OAuth functionality as we won't be using it. 
+            emailClient.AuthenticationMechanisms.Remove("XOAUTH2");
+
+            emailClient.Authenticate(_emailConfiguration.SmtpUsername, _emailConfiguration.SmtpPassword);
+
+            await emailClient.SendAsync(message);
+
+            emailClient.Disconnect(true);
+        }
+    }
+}
